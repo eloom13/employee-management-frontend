86817b92f053b9c8d540becbc8dbef62a1e6b338
Elmir Mujkić <116904225+eloom13@users.noreply.github.com>
2024-09-10 16:24:09 +0200

    finish service for backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], // Add CommonModule
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employee-manager-app';
+export class AppComponent implements OnInit {
+  public employees: Employee[] | undefined;
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.error('Error fetching employees:', error.message);
+      }
+    );
+  }
 }
